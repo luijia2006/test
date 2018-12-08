382364cd06ded41b4c9abfa5aa74ac6074ab7010
liujian <346643585@qq.com>
2018-12-08 17:38:25 +0800

    增加netty模拟websocket测试demo

---
+++ src/main/java/com/we/netty/Global.java
@@ -0,0 +1,13 @@
+package com.we.netty;
+
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+/**
+ * ClassName:Global
+ * Function: TODO ADD FUNCTION.
+ */
+public class Global {
+    public static ChannelGroup group = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+}

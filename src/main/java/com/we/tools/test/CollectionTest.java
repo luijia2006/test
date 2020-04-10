81d80d51f9c3cca0cd496d9f2e40a79d0d62b523
liujian <346643585@qq.com>
2020-04-10 16:47:46 +0800

    list排重

--- src/main/java/com/we/tools/test/CollectionTest.java
+++ src/main/java/com/we/tools/test/CollectionTest.java
@@ -1,16 +1,64 @@
 package com.we.tools.test;
 
 import com.we.core.common.util.CollectionUtil;
-import com.we.core.common.util.Util;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class CollectionTest {
     public static void main(String[] args) {
         //是否是空方法测试
-        List list = CollectionUtil.list();
-        if (Util.isEmpty(list)) {
-            System.out.println("true");
-        }
+        check();
     }
+
+
+    public static void check(){
+
+        List<BookCategoryDto> list = CollectionUtil.list();
+        list.add(new BookCategoryDto(1,"书籍1"));
+        list.add(new BookCategoryDto(2,"书籍1"));
+
+        List<BookCategoryDto> uniquelist = list.stream().collect(
+                Collectors.collectingAndThen(Collectors.toCollection(() -> new TreeSet<>(Comparator.comparing(BookCategoryDto::getName))), ArrayList::new)
+        );
+
+        System.out.println(uniquelist);
+
+    }
+}
+
+class BookCategoryDto{
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+    public BookCategoryDto() {
+
+    }
+
+    public BookCategoryDto(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
 }
